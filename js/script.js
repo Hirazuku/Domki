d7a036e78e3201de472d40a3b7f9cf2a4230699c
Michał Kowalczyk <michalkowal3k@gmail.com>
2024-03-01 21:07:40 +0100

    dark mode buttons and article made visible

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 {
     const onToggleBackgroundClick = () => {
         const body = document.querySelector(".container")
+        const article = document.querySelector(".article")
+        const button = document.querySelector(".button")
+        const button2 = document.querySelector(".button2")
+        const button3 = document.querySelector(".button3")
         const themeName = document.querySelector(".themeName")
         body.classList.toggle("dark");
+        article.classList.toggle("dark");
+        button.classList.toggle("dark");
+        button2.classList.toggle("dark");
+        button3.classList.toggle("dark");
         themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
     };
 
